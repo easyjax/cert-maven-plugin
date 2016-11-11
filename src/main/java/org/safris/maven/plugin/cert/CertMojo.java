1b2436332d1de083399b9010928d354821522219
Seva Safris <seva@safris.org>
2016-11-10 23:13:02 -0800

    Made maven.plugin names conform to naming compliance.

---
+++ src/main/java/org/safris/maven/plugin/cert/CertMojo.java
@@ -0,0 +1,89 @@
+/* Copyright 2006 Sun Microsystems, Inc.  All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Sun Microsystems nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, final STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.safris.maven.plugin.cert;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.maven.model.Repository;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.safris.maven.common.AdvancedMojo;
+
+@Mojo(name = "import", defaultPhase = LifecyclePhase.COMPILE)
+@Execute(goal = "import")
+public final class CertMojo extends AdvancedMojo {
+  public static final Set<String> checkedURLs = new HashSet<String>();
+
+  @Parameter(property = "project.repositories", readonly = true, required = true)
+  private boolean mavenTestSkip;
+  private List<Repository> repositories;
+
+  public List<Repository> getRepositories() {
+    return repositories;
+  }
+
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    for (final Repository repository : getRepositories()) {
+      if (repository.getUrl().startsWith("https") && !checkedURLs.contains(repository.getUrl())) {
+        try {
+          final URL url = new URL(repository.getUrl());
+          getLog().info(url.getHost() + ":" + url.getPort());
+          String arg = url.getHost();
+          if (url.getPort() != -1)
+            arg += ":" + url.getPort();
+
+          InstallCert.main(new String[] {arg});
+          checkedURLs.add(repository.getUrl());
+        }
+        catch (final FileNotFoundException e) {
+          if (!e.getMessage().contains("(Permission denied)"))
+            throw new MojoFailureException("Failure due to InstallCert", e);
+
+          getLog().error("Attempting to modify JDK CA certificates file " + e.getMessage());
+          getLog().error("Please run the same command as root, via \"sudo\".");
+          System.exit(0);
+        }
+        catch (final Exception e) {
+          throw new MojoExecutionException("Failure due to InstallCert", e);
+        }
+      }
+    }
+  }
+}
---
+++ src/main/java/org/safris/maven/plugin/cert/KeyStoreImport.java
@@ -0,0 +1,101 @@
+//
+//KeyStoreImport.java
+//
+//Adds a specified certificate chain and associated RSA private key
+//to a Java keystore.
+//
+//Usage: java KeyStoreImport KEYSTORE CERTS KEY ALIAS
+//
+//           KEYSTORE is the name of the file containing the Java keystore
+//           CERTS is the name of a file containing a chain of concatenated
+//                   DER-encoded X.509 certificates
+//           KEY is the name of a file containing a DER-encoded PKCS#8 RSA
+//                   private key
+//           ALIAS is the alias for the private key entry in the keystore
+//
+//Â©Neal Groothuis
+//2006-08-08
+//
+//This program is free software; you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation; either version 2 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+package org.safris.maven.plugin.cert;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.security.KeyFactory;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+public class KeyStoreImport {
+  public static void main(final String args[]) throws Exception {
+    // Meaningful variable names for the arguments
+    final String keyStoreFileName = args[0];
+    final String certificateChainFileName = args[1];
+    final String privateKeyFileName = args[2];
+    final String entryAlias = args[3];
+    final String keyStorePassword = args[4];
+    final String privateKeyEntryPassword = args[5];
+
+    final File keyFile = new File(privateKeyFileName);
+    try (
+      final FileInputStream certificateStream = new FileInputStream(certificateChainFileName);
+      final FileInputStream keyInputStream = new FileInputStream(keyFile);
+    ) {
+      final byte[] key = new byte[(int)keyFile.length()];
+      keyInputStream.read(key);
+
+      final KeyStore keyStore;
+      if (keyStoreFileName != null) {
+        keyStore = KeyStore.getInstance("jks");
+        try (final FileInputStream keyStoreInputStream = new FileInputStream(keyStoreFileName)) {
+          keyStore.load(keyStoreInputStream, keyStorePassword.toCharArray());
+        }
+
+        makeKeystore(keyStore, certificateStream, key, entryAlias, privateKeyEntryPassword);
+
+        try (final FileOutputStream keyStoreOutputStream = new FileOutputStream(keyStoreFileName)) {
+          keyStore.store(keyStoreOutputStream, keyStorePassword.toCharArray());
+        }
+      }
+      else {
+        keyStore = makeKeystore(null, certificateStream, key, entryAlias, privateKeyEntryPassword);
+      }
+    }
+  }
+
+  public static KeyStore makeKeystore(final KeyStore defaultKeystore, final InputStream certificateChainInputStream, final byte[] key, final String entryAlias, final String privateKeyEntryPassword) throws Exception {
+    final KeyStore keyStore = defaultKeystore != null ? defaultKeystore : KeyStore.getInstance("jks");
+
+    // Load the certificate chain (in X.509 DER encoding).
+    final CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+    // Required because Java is STUPID. You can't just cast the result
+    // of toArray to Certificate[].
+    Certificate[] chain = {};
+    chain = certificateFactory.generateCertificates(certificateChainInputStream).toArray(chain);
+
+    // Load the private key (in PKCS#8 DER encoding).
+    final KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+    final PrivateKey privateKey = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(key));
+
+    // Add the new entry
+    keyStore.setEntry(entryAlias, new KeyStore.PrivateKeyEntry(privateKey, chain), new KeyStore.PasswordProtection(privateKeyEntryPassword.toCharArray()));
+    return keyStore;
+  }
+}
