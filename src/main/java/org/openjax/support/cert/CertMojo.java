248ef94d64a103b108b90e39d045826702cb1d74
Seva Safris <seva@safris.org>
2019-04-28 01:05:55 +0200

    Repackage to ext

--- src/main/java/org/openjax/support/cert/CertMojo.java
+++ src/main/java/org/openjax/support/cert/CertMojo.java
@@ -31,7 +31,7 @@ import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.openjax.standard.maven.mojo.BaseMojo;
+import org.openjax.ext.maven.mojo.BaseMojo;
 
 @Mojo(name="import", defaultPhase=LifecyclePhase.INITIALIZE)
 @Execute(goal="import")
